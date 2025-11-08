ecaaac7822b94bc92f40cec41d83bfdeb25f2352
VHeurtevant <murasa.chan@gmail.com>
2025-11-08 16:24:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,12 @@ def reachable(graph, start_node):
     result = set([start_node])
     frontier = set([start_node])
     while len(frontier) != 0:
-        ###TODO
-        pass
+      node = frontier.pop() #Visits and removes most recent node
+      for neighbor in graph[node]: #Checks all nodes adjacent to current node
+          if neighbor not in result: # Only add those not in result to frontier, prevents redundancy
+                result.add(neighbor)
+                frontier.add(neighbor)
+       
     return result
 
 
@@ -26,9 +30,10 @@ def reachable(graph, start_node):
 
 
 def connected(graph):
-    ### TODO
-    pass
-
+    start_node = min(graph)  #Picks smallest node to start, but any node can be picked since we are working with undirected graph
+    reachable_nodes = reachable(graph, start_node)
+    
+    return len(reachable_nodes) == len(graph) #Returns true if same length (reachable set same as graph implies all nodes reached), false otherwise
 
 
 
@@ -37,6 +42,15 @@ def n_components(graph):
     Returns:
       the number of connected components in an undirected graph
     """
-    ### TODO
-    pass
+    unvisited = set(graph.keys()) #Initializes set with all nodes
+    count = 0
+    
+    while unvisited:
+        node = unvisited.pop()           # Pick an arbitrary unvisited node
+        reachable_nodes = reachable(graph, node)
+        unvisited -= reachable_nodes     # Remove all nodes in this component to avoid redundant calls
+        count += 1 #Iterate num components
+    
+    return count
+    
 
